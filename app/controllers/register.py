0056941d6e36c852a83dae8e8bfe403adff8f716
Igor-Felipy <igor.felipy123@gmail.com>
2020-08-20 18:11:11 -0400

    criando a view e o controller do register

--- app/controllers/register.py
+++ app/controllers/register.py
@@ -1,5 +1,15 @@
 from . import controllers
-
-@controllers.route('/register')
+from flask import request, render_template
+from ..db import register_db
+@controllers.route('/register', methods=['GET','POST'])
 def register():
-    return 'register page'
+    if request.method == 'POST':
+        data = []
+        data.append(request.form.get("email"))
+        data.append(request.form.get("name"))
+        data.append(request.form.get("nick"))
+        data.append(request.form.get("password"))
+        register_db.new_user(data)
+        
+    else:
+        return render_template('request.html')
